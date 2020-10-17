c410800eac1c307602c3e559cf7f95586fb6fb4a
geno nullfree nullfree.geno@gmail.com <nullfree.geno@gmail.com>
2020-10-16 22:46:55 -0400

    adding boilerplate

--- src/puppetmaster.c
+++ src/puppetmaster.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
+    FILE *child;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: %s [executable]\n", argv[0]);
+        return 1;
+    }
+
+    if (( child = popen(argv[1], "w")) == NULL)
+    {
+        perror("popen");
+        return 1;
+    }
+
     printf("hello world\n");
+
+    pclose(child);
 
     return 0;
 }
