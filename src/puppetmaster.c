555115ccf3e4de83d707f3b22e0262d09a73b8d8
geno nullfree nullfree.geno@gmail.com <nullfree.geno@gmail.com>
2020-10-16 23:05:08 -0400

    reworked with pipes

--- src/puppetmaster.c
+++ src/puppetmaster.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 int main(int argc, char *argv[])
 {
     FILE *child;
+    int pid;
+    int p[2];   /* parent: r, w -- parent reads from child */
+    int c[2];   /*  child: r, w -- child reads from parent*/
+    int nbytes;
 
     if (argc != 2)
     {
@@ -11,15 +16,25 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    if (( child = popen(argv[1], "w")) == NULL)
+    pipe(p);
+    pipe(c);
+
+    if (fork() == -1)
     {
-        perror("popen");
+        perror("fork");
         return 1;
     }
+    else if (pid == 0) /* child */
+    {
+        dup2(0, p[0]);
+        dup2(1, c[1]);
 
-    printf("hello world\n");
+        return 0;
+    }
 
-    pclose(child);
+    /* parent */
+    dup2(1, p[1]);
+    dup2(0, c[0]);
 
     return 0;
 }
