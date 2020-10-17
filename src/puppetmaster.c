dad031380012a86a4a9019a9fc1c962237a98b67
geno nullfree nullfree.geno@gmail.com <nullfree.geno@gmail.com>
2020-10-16 23:24:16 -0400

    almost kinda working

--- src/puppetmaster.c
+++ src/puppetmaster.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 
@@ -19,22 +20,37 @@ int main(int argc, char *argv[])
     pipe(p);
     pipe(c);
 
-    if (fork() == -1)
+    if ((pid = fork()) == -1)
     {
         perror("fork");
         return 1;
     }
     else if (pid == 0) /* child */
     {
-        dup2(0, p[0]);
-        dup2(1, c[1]);
+        dup2(0, c[0]);
+        dup2(1, p[1]);
 
-        return 0;
+        execve(argv[1], argv+1, NULL);
+        perror("execve");
+        return 1;
     }
 
     /* parent */
-    dup2(1, p[1]);
-    dup2(0, c[0]);
+    dup2(1, c[1]);
+    dup2(0, p[0]);
+
+    char *buffer = (char*)calloc(sizeof(char),4096);
+    if (buffer == NULL)
+    {
+        perror("buffer");
+        return 1;
+    }
+
+    nbytes = read(p[0], buffer, 4095);
+    if (nbytes > 0)
+    {
+        printf("captured buffer: [%s]\n", buffer);
+    }
 
     return 0;
 }
